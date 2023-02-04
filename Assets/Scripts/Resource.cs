e68ce8d11a0626dd74ff9cf689de5972c22fa3f2
javier izquierdo <izquierdovi@esat-alumni.com>
2023-02-04 16:28:04 +0100

    resource dispear

--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -6,16 +6,24 @@ public class Resource : MonoBehaviour
 {
   public Hud hud_info_;
   public int on_pick_resources_ = 5;
+  public float TimeAlive;
+
+  private float aliveTimer;
   // Start is called before the first frame update
   void Start()
   {
     hud_info_ = GameObject.FindObjectOfType<Hud>();
+    aliveTimer = TimeAlive;
   }
 
   // Update is called once per frame
   void Update()
   {
-      
+        aliveTimer -= Time.deltaTime;
+    if (aliveTimer <= 0)
+    {
+      Destroy(gameObject);
+    }
   }
 
   void OnTriggerEnter(Collider other){
