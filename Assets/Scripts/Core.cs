c374fc702602b972a7311ac5e236c813b94bea94
adrisangar <sanchezgar@esat-alumni.com>
2023-02-04 18:18:35 +0100

    Added test scene to receive feedback

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Core : MonoBehaviour
 {
   [SerializeField] private int health_ = 10;
+  public Hud hud_info_;
   void Start()
   {
       
@@ -13,7 +14,7 @@ public class Core : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-      
+    hud_info_.core_health_ = health_;
   }
 
   public void damageCore(int dmg){
--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -9,7 +9,9 @@ public class Hud : MonoBehaviour
   public TextMeshProUGUI resource_one_;
   public TextMeshProUGUI resource_two_;
   public TextMeshProUGUI resource_three_;
+  public TextMeshProUGUI core_status_;
   public Vector3 resources_inv_;
+  public int core_health_;
   // Start is called before the first frame update
   void Start()
   {
@@ -22,6 +24,7 @@ public class Hud : MonoBehaviour
     resource_one_.text = resources_inv_.x.ToString();
     resource_two_.text = resources_inv_.y.ToString();
     resource_three_.text = resources_inv_.z.ToString();
+    core_status_.text = core_health_.ToString();
   }
 
  
